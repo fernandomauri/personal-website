5f3db550fc0b0c657e5ba5ff5bd3f6440b1b5597
fernandomauri <fernandomauriii@gmail.com>
2025-12-19 00:43:54 -0800

    Major updates and fixes

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
-window.onload = function() {
-    block();
-    popUp();
-    // changeCursor();
-    // changeTitle();
-}
-
-// function changeTitle() {
-//     titles = ["whatsmyspace", "(5) New Messages"];
-//     for (i=1; i<titles.length; i++) {
-//             document.title = titles[i];
-//             --i
-//     };
-// }
-
-function changeCursor() {
-    let buttons = document.getElementsByClassName("contact-section-button");
-        buttons.addEventListener("hover", function(){
-            buttons.style.cursor = 'url(images/cursor-natalie-nunn-chin.png)';
-    })
-}
-
 function block() {
     /* Simulate blocking me by pressing the Block button */
-    let blockButton = document.getElementById("block-button");
-    let buttonText = document.getElementById("block-button-text");
-    blockButton.addEventListener("click", function() {
-        if (buttonText.textContent === "Block User") {
-            document.getElementById("block-button-text").textContent = "Unblock User";
+    let blockButtonText = document.getElementById("block-button-text");
+    if (blockButtonText.innerHTML === "Block User") {
+        let blockPopUp = document.getElementById('block-popup');
+        blockPopUp.showModal();
+        let confirmBlock = document.getElementById('confirm-block');
+        confirmBlock.addEventListener('click', () => {
+            document.getElementById("block-button-text").innerHTML = "Unblock User";
             document.getElementById("body-box").style.backgroundImage = 'url(images/backgrounds/blizzocked.png)';
-        } else {
-            document.getElementById("block-button-text").textContent = "Block User";
-            document.getElementById("body-box").style.backgroundImage = "none";
-        }
-    })
+        })
+    } else {
+        document.getElementById("block-button-text").innerHTML = "Block User";
+        document.getElementById("body-box").style.backgroundImage = "none";
+    }
 }
 
-function rotateAds() {
-    let fakeAds = ['images/ad-altima.png', 'images/ad-veneer-tech'];
-    let ad = document.getElementById('ad');
+function callImPopUp() {
+    let dialogPopup = document.getElementById('hit-me-up-popup');
+    randomColors(dialogPopup);
+    dialogPopup.showModal();
 }
 
-function popUp() {
-    let dialogPopup = document.getElementById('hit-me-up');
-    let hmuBox = document.getElementById('call-im-button');
+function closePopUp(popUp) {
+    let dialogPopup = document.getElementById(popUp);
+    dialogPopup.close()
+}
 
-    hmuBox.addEventListener('click', function() {
-        dialogPopup.showModal();
-    })
+function headerButtonPopUp(popUp) {
+    let dialogPopup = document.getElementById(popUp);
+    randomColors();
+    dialogPopup.showModal();
+}
+
+function randomColors() {
+    /* I wanted to use the Pantone "Color of the Year" palette to randomly swap popup background colors.
+    Colors in order: Cannoli Cream, Cream Tan, Safari
+    */
+    const choices = [
+        '#f1f0e2',
+        '#E4C7B7',
+        '#baaa91'
+    ];
+    let randomNumber = Math.floor(Math.random() * 3);
+    let chosen = choices[randomNumber];
+    console.log('Chosen Pantone color is ' + chosen);
+    let popUp = document.getElementById('header-button-pop-up');
+    popUp.style.backgroundColor = chosen;
 }
 
 function copyLink() {
     navigator.clipboard.writeText('https://linkedin.com/in/fernandomauri');
-    let dialogPopup = document.getElementById('copied-to-clipboard');
+    let dialogPopup = document.getElementById('copied-to-clipboard-popup');
+    randomColors(dialogPopup);
     dialogPopup.showModal();
 }
